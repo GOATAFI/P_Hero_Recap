06c511f1f5aa9dff594d682b76b6f3e60d5adf48
GOATAFI <projectexecutiveofpm@gmail.com>
2024-11-18 21:53:33 +0600

    fetching with displaying data

--- fetch.js
+++ fetch.js
@@ -1,3 +1,21 @@
 fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then((response) => response.json())
   .then((json) => console.log(json));
+
+const loadUsers = () => {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data) => displayUsers(data));
+};
+
+const displayUsers = (data) => {
+  //   console.log(data);
+  const display = document.getElementById("users_list");
+  //   display.innerHTML = data[0].name;
+  for (let displayData of data) {
+    console.log(displayData);
+    const li = document.createElement("li");
+    li.innerText = displayData.name;
+    display.appendChild(li);
+  }
+};
