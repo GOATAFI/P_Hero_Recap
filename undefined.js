5491f73a8761a0da717f57eea59d68a46d74a233
GOATAFI <projectexecutiveofpm@gmail.com>
2024-11-18 12:48:32 +0600

    undefined

---
+++ undefined.js
@@ -0,0 +1,12 @@
+/**
+ * 8 ways to get undefined
+ * 1.variable that is not initialized will give undefined
+ * 2.function with no return
+ * 3.parameter that is not passed will be undefined
+ * 4.if return has nothing on the right side will return undefined
+ * 5.property that doesn't exists on an object will give you undefined
+ * 6.accessing array elements outside of the index range
+ * 7.deleting an array inside an array
+ * 8.set a value directly to undefined
+ *
+ */
